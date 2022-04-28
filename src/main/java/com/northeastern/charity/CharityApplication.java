3c0fb056b3b28e0bdee6df0ffd9a74779616c20a
Achyuth Varma <achyuthvarma@Achyuths-MacBook-Pro.local>
2022-04-28 13:09:49 -0400

    Added basic JForm into Spring Application

--- src/main/java/com/northeastern/charity/CharityApplication.java
+++ src/main/java/com/northeastern/charity/CharityApplication.java
@@ -1,13 +1,22 @@
 package com.northeastern.charity;
 
-import org.springframework.boot.SpringApplication;
+import com.northeastern.charity.gui.MainJForm;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class CharityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CharityApplication.class, args);
-	}
+    public static void main(String[] args) {
+//        SpringApplication.run(CharityApplication.class, args);
+        ConfigurableApplicationContext configurableApplicationContext = new SpringApplicationBuilder(CharityApplication.class).headless(false).run(args);
+        EventQueue.invokeLater(() -> {
+            MainJForm ex = configurableApplicationContext.getBean(MainJForm.class);
+            ex.setVisible(true);
+        });
+    }
 
 }
